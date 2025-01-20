1437be40c7b581e9dd04d231bbd9aa82c4ae2307
gr00ze <vincenzop23@gmail.com>
2025-01-20 18:19:49 +0100

    Modified generic given setting instance to generic method of relative item class

--- src/main/java/com/gr00ze/training/item/ItemList.java
+++ src/main/java/com/gr00ze/training/item/ItemList.java
@@ -8,8 +8,8 @@ import static com.gr00ze.training.util.RegisterFunctions.*;
 public class ItemList{
  public static final Item TRAINING_ITEM = registerItem("training_item", Item::new, new Item.Settings());
  public static final Item TRAINING_BLOCK_ITEM = registerItem(new Item.Settings(), id("training_block_item"), TRAINING_BLOCK);
- public static final Item TRAINING_CUSTOM_ITEM = registerItem("training_custom_item", TrainingCustomItem::new, new Item.Settings());
- public static final Item TRAINING_SOUND_ITEM = registerItem("training_sound_item", TrainingSoundItem::new ,new Item.Settings());
+ public static final Item TRAINING_CUSTOM_ITEM = registerItem("training_custom_item", TrainingCustomItem::new, TrainingCustomItem.getItemSetting());
+ public static final Item TRAINING_SOUND_ITEM = registerItem("training_sound_item", TrainingSoundItem::new ,TrainingSoundItem.getItemSetting());
 
  public static void initialize(){}
 
