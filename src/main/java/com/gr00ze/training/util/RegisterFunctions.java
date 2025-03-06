5ffae6eef55122684e70e38e8e68cd0624b7a7d3
gr00ze <vincenzop23@gmail.com>
2025-03-06 19:55:07 +0100

    ChatGPT said "Shit on Functional Interface, use Function class"

--- src/main/java/com/gr00ze/training/util/RegisterFunctions.java
+++ src/main/java/com/gr00ze/training/util/RegisterFunctions.java
@@ -20,6 +20,8 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 
+import java.util.function.Function;
+
 import static com.gr00ze.training.Training.MOD_ID;
 
 
@@ -39,18 +41,13 @@ public class RegisterFunctions {
         return Registry.register(Registries.BLOCK, key, block);
     }
     //RIGHT
-    @FunctionalInterface
-    public interface BlockFactory<B extends Block> {
-        B create(Block.Settings settings);
-    }
-    public static Block registerBlock(String blockId, BlockFactory<Block> factory , AbstractBlock.Settings settings){
+
+    public static Block registerBlock(String blockId, Function<AbstractBlock.Settings, Block> factory , AbstractBlock.Settings settings){
         Identifier id = id(blockId);
         RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, id);
         settings.registryKey(key);
-        Block block = factory.create(settings);
+        Block block = factory.apply(settings);
         return Registry.register(Registries.BLOCK, key, block);
-
-
     }
 
     public static <B extends BlockEntity> BlockEntityType<B> registerBlockEntity(String blockEntityId, FabricBlockEntityTypeBuilder.Factory<B> factory, Block ...blocks){
@@ -72,14 +69,9 @@ public class RegisterFunctions {
         return Registry.register(Registries.ITEM, key, item);
     }
 
-    @FunctionalInterface
-    public interface ItemFactory<I extends Item> {
-        I create(Item.Settings settings);
-    }
-
-    public static <I extends Item> Item registerItem(String itemName, ItemFactory<I> itemFactory, Item.Settings settings) {
+    public static <I extends Item> Item registerItem(String itemName, Function<Item.Settings, I> factory, Item.Settings settings) {
         RegistryKey<Item> key = registerItemKey(settings, id(itemName));
-        I item = itemFactory.create(settings);
+        I item = factory.apply(settings);
         return Registry.register(Registries.ITEM, key, item);
     }
 
@@ -96,7 +88,7 @@ public class RegisterFunctions {
         Identifier identifier = id(entityID);
         RegistryKey<EntityType<?>> key = RegistryKey.of(RegistryKeys.ENTITY_TYPE, identifier);
         return Registry.register(Registries.ENTITY_TYPE, key, entityBuilder.build(key));
-    };
+    }
     
     public static void registerMobAttribute(EntityType<? extends MobEntity> entityType, DefaultAttributeContainer.Builder builder){
         FabricDefaultAttributeRegistry.register(entityType, builder.build());
@@ -105,6 +97,7 @@ public class RegisterFunctions {
     public static SoundEvent registerSound(String soundName){
         Identifier identifier = id(soundName);
         RegistryKey<SoundEvent> key = RegistryKey.of(RegistryKeys.SOUND_EVENT, identifier);
+        // MA LA KEY LA DEVO USARE?????
         return Registry.register(Registries.SOUND_EVENT, identifier, SoundEvent.of(identifier));
     }
 }
