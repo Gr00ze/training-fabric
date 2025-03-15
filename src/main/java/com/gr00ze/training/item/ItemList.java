6f648295f50dc1b21cb85784993dfdfd93de7a67
gr00ze <vincenzop23@gmail.com>
2025-03-15 23:50:37 +0100

    Added Comment

--- src/main/java/com/gr00ze/training/item/ItemList.java
+++ src/main/java/com/gr00ze/training/item/ItemList.java
@@ -6,10 +6,16 @@ import static com.gr00ze.training.BlockList.*;
 import static com.gr00ze.training.util.RegisterFunctions.*;
 
 public class ItemList{
- public static final Item TRAINING_ITEM = registerItem("training_item", Item::new, new Item.Settings());
- public static final Item TRAINING_BLOCK_ITEM = registerItem( "training_block_item",new Item.Settings(), TRAINING_BLOCK);
- public static final Item TRAINING_CUSTOM_ITEM = registerItem("training_custom_item", TrainingCustomItem::new, TrainingCustomItem.getItemSetting());
- public static final Item TRAINING_SOUND_ITEM = registerItem("training_sound_item", TrainingSoundItem::new ,TrainingSoundItem.getItemSetting());
+ //SIMPLE ITEM WITH SIMPLE SETTINGS
+ public static final Item SIMPLE_ITEM = registerItem("simple_item", Item::new, new Item.Settings());
+ //ITEM WITH CUSTOM CLASS AND CUSTOM SETTINGS
+ public static final Item CUSTOM_ITEM = registerItem("custom_item", TrainingCustomItem::new, TrainingCustomItem.getItemSetting());
+ //CUSTOM ITEM WITH SOUND BEHAVIOUR
+ public static final Item CUSTOM_SOUND_ITEM = registerItem("custom_sound_item", TrainingSoundItem::new ,TrainingSoundItem.getItemSetting());
+
+ //ITEM TO PLACE A BLOCK WITH SIMPLE SETTINGS
+ public static final Item SIMPLE_BLOCK_ITEM = registerItem( "simple_block_item",new Item.Settings(), SIMPLE_BLOCK);
+
 
  public static void initialize(){}
 
