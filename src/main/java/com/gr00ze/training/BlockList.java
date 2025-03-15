d0379857a0a6598dd2f77dc25d0894a72ada67b9
gr00ze <vincenzop23@gmail.com>
2025-03-15 23:49:10 +0100

    Renaming and commenting

--- src/main/java/com/gr00ze/training/BlockList.java
+++ src/main/java/com/gr00ze/training/BlockList.java
@@ -2,23 +2,27 @@ package com.gr00ze.training;
 
 import com.gr00ze.training.block.TrainingCustomBlock;
 import com.gr00ze.training.block.TrainingCustomBlockWithEntity;
-import com.gr00ze.training.block.TrainingCustomBlockEntity;
+import com.gr00ze.training.block.blockentity.TrainingCustomBlockEntity;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.Item;
 
-
-import static com.gr00ze.training.util.RegisterFunctions.registerBlock;
-import static com.gr00ze.training.util.RegisterFunctions.registerBlockEntity;
+import static com.gr00ze.training.util.RegisterFunctions.*;
 
 public class BlockList {
 
     public static final Block
-        TRAINING_BLOCK = registerBlock("training_block", AbstractBlock.Settings.create()),
-        TRAINING_CUSTOM_BLOCK = registerBlock("training_custom_block", TrainingCustomBlock::new, TrainingCustomBlock.getBlockSettings()),
-        TRAINING_BLOCK_WITH_ENTITY = registerBlock("training_custom_block_with_entity", TrainingCustomBlockWithEntity::new, TrainingCustomBlockWithEntity.getBlockSettings());
+        //SIMPLE BLOCK WITH SIMPLE SETTINGS
+        SIMPLE_BLOCK = registerBlock("simple_block", AbstractBlock.Settings.create()),
+        //BLOCK WITH CUSTOM CLASS AND CUSTOM SETTINGS AND ITEM WITH DEFAULT SETTINGS
+        CUSTOM_BLOCK = registerBlockAndItem("custom_block", TrainingCustomBlock::new, TrainingCustomBlock.getBlockSettings(),new Item.Settings()),
+        //BLOCK WITH BLOCK ENTITY AND CUSTOM SETTINGS
+        CUSTOM_BLOCK_WITH_ENTITY = registerBlockAndItem("custom_block_with_entity", TrainingCustomBlockWithEntity::new, TrainingCustomBlockWithEntity.getBlockSettings(), new Item.Settings());
 
-    public static final BlockEntityType<TrainingCustomBlockEntity> TRAINING_BLOCK_ENTITY = registerBlockEntity("training_custom_block_entity",TrainingCustomBlockEntity::new, TRAINING_BLOCK_WITH_ENTITY);
+
+    //BLOCK ENTITY
+    public static final BlockEntityType<TrainingCustomBlockEntity> CUSTOM_BLOCK_ENTITY = registerBlockEntity("custom_block_entity",TrainingCustomBlockEntity::new, CUSTOM_BLOCK_WITH_ENTITY);
 
 
 
