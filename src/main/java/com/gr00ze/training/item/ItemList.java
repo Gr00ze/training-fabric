0c02df1f368d51be9751ab4a14aafd4d7963b7ec
gr00ze <vincenzop23@gmail.com>
2025-03-17 00:14:42 +0100

    Renaming variables

--- src/main/java/com/gr00ze/training/item/ItemList.java
+++ src/main/java/com/gr00ze/training/item/ItemList.java
@@ -7,19 +7,19 @@ import static com.gr00ze.training.util.RegisterFunctions.*;
 
 public class ItemList{
  //SIMPLE ITEM WITH SIMPLE SETTINGS
- public static final Item SIMPLE_ITEM = registerItem("simple_item", Item::new, new Item.Settings());
+ public static final Item SIMPLE_ITEM = registerItem("simple_item");
  //ITEM WITH CUSTOM CLASS AND CUSTOM SETTINGS
  public static final Item CUSTOM_ITEM = registerItem("custom_item", TrainingCustomItem::new, TrainingCustomItem.getItemSetting());
  //CUSTOM ITEM WITH SOUND BEHAVIOUR
  public static final Item CUSTOM_SOUND_ITEM = registerItem("custom_sound_item", TrainingSoundItem::new ,TrainingSoundItem.getItemSetting());
 
  //ITEM TO PLACE A BLOCK WITH SIMPLE SETTINGS
- public static final Item SIMPLE_BLOCK_ITEM = registerItem( "simple_block_item",new Item.Settings(), SIMPLE_BLOCK);
+ public static final Item SIMPLE_BLOCK_ITEM = registerBlockItem( "simple_block_item",SIMPLE_BLOCK);
+ public static final Item CUSTOM_BLOCK_ITEM = CUSTOM_BLOCK.asItem();
 
+ public static final Item CUSTOM_BLOCK_WITH_ENTITY_ITEM = CUSTOM_BLOCK_WITH_ENTITY.asItem();
+ public static final Item CUSTOM_BLOCK_ALTERNATIVE_ITEM = CUSTOM_BLOCK_ALTERNATIVE.asItem();
 
  public static void initialize(){}
-
-
-
 
 }
